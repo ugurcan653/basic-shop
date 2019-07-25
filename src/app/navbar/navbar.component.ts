bc3556dc5656644e46bb7b5bc4345c6c55d53239
ugur <ugurcancakar@hotmail.com>
2019-07-25 17:58:05 +0300

    remove carts and wishes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,6 +4,7 @@ import { WishService } from '../services/wish.service';
 import { cartProduct } from '../models/cartProduct';
 import { Products } from '../models/products';
 import { Observable } from 'rxjs';
+import { LocalStorageService } from '../services/local-storage.service';
 
 @Component({
   selector: 'app-navbar',
@@ -15,7 +16,7 @@ export class NavbarComponent implements OnInit {
   wishSize: number;
   cartProducts: cartProduct[] = [];
   wishProducts: Products[] = [];
-  constructor(private cartService: CartService, private wishService: WishService) {
+  constructor(private cartService: CartService, private wishService: WishService, private localStorageService: LocalStorageService) {
 
   }
 
@@ -32,5 +33,15 @@ export class NavbarComponent implements OnInit {
   ngDoCheck(): void {
     this.cartSize = this.cartService.getCartCount()
     this.wishSize = this.wishService.getWishCount();
+  }
+  removeWish(deleteproduct){
+    console.log(deleteproduct);
+    this.wishProducts = this.wishProducts.filter(product => product!= deleteproduct);
+    this.localStorageService.setItem('Wish',this.wishProducts);
+    
+  }
+  removeCart(deleteproduct){
+    this.cartProducts = this.cartProducts.filter(product => product!= deleteproduct);
+    this.localStorageService.setItem('Cart',this.cartProducts); 
   }
 }
